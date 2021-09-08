b6ab08cf52dfd36f207c5c26f0ee95047d3bdb8a
Nabin <nkbhaie2017@gmail.com>
2021-09-08 16:52:58 -0600

    add app.py and install flask debugtoolbar

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "OH NO KEY"
+debug = DebugToolbarExtension(app)
 
 @app.route("/")
 def test_flask_working():
