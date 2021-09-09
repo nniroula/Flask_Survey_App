d402e8a947bf135544c8ca9ed52084e8c706b467
Nabin <nkbhaie2017@gmail.com>
2021-09-09 09:47:53 -0600

    add template inheritance, and root route

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "OH NO KEY"
 debug = DebugToolbarExtension(app)
 
+# @app.route("/hello")
+# def test_flask_working():
+#     return "Flask is working"
+
+# step 2
+response = []
+
+title = satisfaction_survey.title
+instructions = satisfaction_survey.instructions
 @app.route("/")
-def test_flask_working():
-    return "Flask is working"
+def show_survey_start_page():
+    return render_template("starter_page.html", survey_title = title, survey_instructions = instructions)
+
+
